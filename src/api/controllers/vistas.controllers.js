46ed54ea32d6a5c290b2dc323304cbcb656dceb8
Lucas Iusef <lucastiusef@gmail.com>
2025-07-08 18:53:14 -0300

    Fix error

--- src/api/controllers/vistas.controllers.js
+++ src/api/controllers/vistas.controllers.js
@@ -20,6 +20,10 @@ export async function insertarDatos(req, res) {
     const campos = Object.keys(req.body);
     const valores = Object.values(req.body);
 
+    if (valores.includes("")) {
+        return res.status(400).send(`<script>alert("Por favor, completa todos los campos."); window.location.href = "/dashboard/admin";</script>`);
+    }
+
     const placeholders = campos.map(() => "?").join(", ");
     const query = `INSERT INTO ${tabla} (${campos.join(", ")}) VALUES (${placeholders})`;
 
@@ -38,6 +42,10 @@ export async function modificarDatos(req, res) {
     const campos = Object.keys(rest);
     const valores = Object.values(rest);
 
+    if (!id) {
+        return res.status(400).send(`<script>alert("Por favor, proporciona un ID válido."); window.location.href = "/dashboard/admin";</script>`);
+    }
+
     const setCampos = campos.map(f => `${f} = ?`).join(", ");
     const query = `UPDATE ${tabla} SET ${setCampos} WHERE id = ?`;
 
@@ -47,17 +55,22 @@ export async function modificarDatos(req, res) {
         if (resultado.affectedRows > 0) {
             res.redirect("/dashboard/admin");
         } else {
-            res.status(404).send(`<script>alert("No se encontró ningún registro con ese ID."); window.location.href = "/admin";</script>`);
+            res.status(404).send(`<script>alert("No se encontró ningún registro con ese ID."); window.location.href = "/dashboard/admin";</script>`);
         };
     } catch (error) {
         console.error(error);
         res.status(500).send("Error al modificar registro");
     };
 }
+
 // Borra los datos de la tabla que haya seleccionado. Si no hay un dato relacionado sale un alert poniendo que no hay nada con ese registro
 export async function borrarDatos(req, res) {
     const tabla = req.params.tabla;
     const { id } = req.body;
+
+    if (!id) {
+        return res.status(400).send(`<script>alert("Por favor, proporciona un ID válido."); window.location.href = "/dashboard/admin";</script>`);
+    }
 
     const query = `UPDATE ${tabla} set ACTIVO = 0 WHERE id = ?`;
 
